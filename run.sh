3602b16786129b7f2ea563eb2b432bfa7f0a6552
Niels Højen <niels@hojen.net>
2018-08-21 12:14:57 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+execServe="/micromdm serve -apns-cert ${APNS_PUSH_CERT} -apns-key ${APNS_PUSH_CERT_KEY} -apns-password='${APNS_PASSWORD}' -server-url='${SERVER_URL}' -filerepo /repo -config-path /config"
+
+# add api key if specified
+if [[ ${API_KEY} ]]; then
+  execServe="${execServe} -api-key ${API_KEY}"
+fi
+
+if [[ ${TLS} = true ]]; then
+  if [[ ! -z ${TLS_CERT} && ! -z ${TLS_KEY} && -e "/certs/${TLS_CERT}" && -e "/certs/${TLS_KEY}" ]]; then
+    execServe="${execServe} -tls-cert '${TLS_CERT}' -tls-key '${TLS_KEY}'"
+  fi
+else
+  execServe="${execServe} -tls=false"
+fi
+
+if [[ ${DEBUG} ]]; then
+  execServe="${execServe} -http-debug"
+fi
+
+echo "Starting using: $execServe"
+
+eval $execServe
