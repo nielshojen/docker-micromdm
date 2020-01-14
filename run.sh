4bf97304625e291fdb88a021b511c5178b781e01
Niels Højen <niels.hojen@bestseller.com>
2020-01-14 11:33:29 +0100

    keep it going

--- run.sh
+++ run.sh
@@ -45,10 +45,12 @@ fi
 
 echo "Starting using: $execServe"
 
-eval $execServe
+eval $execServe &
 
 execMdmctl="/usr/local/bin/mdmctl config set -api-token ${API_KEY} -name production -server-url ${MICROMDM_SERVER_URL}"
 
 echo "Configuring mdmctl using: $execMdmctl"
 
 eval $execMdmctl
+
+sleep infinity
