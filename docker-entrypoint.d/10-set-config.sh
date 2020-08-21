c955dfdd437cf19945af3e446e031e5976723051
Niels Højen <niels.hojen@bestseller.com>
2020-08-21 13:23:32 +0200

    config mdmctl for local

--- docker-entrypoint.d/10-set-config.sh
+++ docker-entrypoint.d/10-set-config.sh
@@ -2,7 +2,14 @@
 
 sleep 40
 
-execMdmctl="/usr/local/bin/mdmctl config set -api-token ${API_KEY} -name production -server-url ${MICROMDM_SERVER_URL}"
+if [[ ${TLS} = true ]]; then
+  SERVER_URL="http://localhost:443"
+else
+  SERVER_URL="http://localhost:8080"
+fi
+
+
+execMdmctl="/usr/local/bin/mdmctl config set -api-token ${API_KEY} -name production -server-url ${SERVER_URL}"
 
 echo "Configuring mdmctl using: $execMdmctl"
 
