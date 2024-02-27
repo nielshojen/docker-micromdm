294d57e1c3ccfe1782b594279709e93ff3949f8e
Niels Højen <niels@hojen.net>
2024-02-27 08:43:13 +0100

    Update 00-start-micrmdm.sh

--- docker-entrypoint.d/00-start-micrmdm.sh
+++ docker-entrypoint.d/00-start-micrmdm.sh
@@ -2,4 +2,11 @@
 
 execServe="/usr/local/bin/micromdm serve"
 
+if [[ ${MICROMDM_LOG_TIME} = "truew" ]]; then
+  sleep 5
+  execServe="${execServe} -log-time"
+  echo "Including timestamp in log messages: $execServe"
+  $execMdmctl
+fi
+
 $execServe &
